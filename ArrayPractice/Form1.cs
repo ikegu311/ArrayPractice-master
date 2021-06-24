84dabcbeaeb6a0ab4ae3bccfe50669921519ba7c
A601-05 <tnk.dat.class@gmail.com>
2021-06-24 09:52:23 +0900

    5

--- ArrayPractice/Form1.cs
+++ ArrayPractice/Form1.cs
@@ -112,8 +112,25 @@ namespace ArrayPractice
                 && (fpos.Y >= label1.Top)
                 && (fpos.Y < label1.Bottom))
             {
-                timer1.Enabled = false;
+                label1.Visible = false;
             }
+
+            if ((fpos.X >= label2.Left)
+                && (fpos.X < label2.Right)
+                && (fpos.Y >= label2.Top)
+                && (fpos.Y < label2.Bottom))
+            {
+                label2.Visible = false;
+            }
+
+            if ((fpos.X >= label3.Left)
+                && (fpos.X < label3.Right)
+                && (fpos.Y >= label3.Top)
+                && (fpos.Y < label3.Bottom))
+            {
+                label3.Visible = false;
+            }
+
         }
 
         private void label3_Click(object sender, EventArgs e)
